abe9b0b3a42f00dc4f3e0093773ae348775f4ad0
Joey Chung K <joeychung960@gmail.com>
2016-09-01 05:30:19 -0700

    Added in an auto-refresher when it detects a visual erroron screen

--- IW.sikuli/IW.py
+++ IW.sikuli/IW.py
@@ -1,5 +1,11 @@
 #Press Shift+Alt+C to escape
 while True:
+    if(Region(817,520,233,198).exists(Pattern("Error.png").similar(0.80)) or Region(754,439,381,276).exists("Error2.png")):
+        type(Key.F5)
+        wait(10)
+        if(Region(513,595,328,120).exists("StartName.png")):
+            click("StartName.png")
+            wait(5)            
     #Look for new co-ops
     if(Region(441,400,81,76).exists(Pattern("NewComboCoOp.png").similar(0.80))):
         wait(.1)
@@ -8,19 +14,14 @@ while True:
             newText = find(Pattern("New.png").similar(0.95))
             newRegion = Region(newText.getX(),newText.getY(),220,155)
             #Need to make an escape case here
-            newRegion.click("GoHelp.png")
-            if(Region(597,561,237,98).exists("Persuade.png")):
-                Region(597,561,237,98).click("Persuade.png")
-                wait(3)
-                if(Region(897,252,126,84).exists(Pattern("SKIP.png").similar(0.50))):
-                    Region(897,252,126,84).click(Pattern("SKIP.png").similar(0.50))
-                else:
-                    continue
-            else:
-                continue
-        else:
-            continue
-        wait(2)
+            if(newRegion.exists("GoHelp.png")):
+                newRegion.click("GoHelp.png")
+                if(Region(597,561,237,98).exists("Persuade.png")):
+                    Region(597,561,237,98).click("Persuade.png")
+                    wait(3)
+                    if(Region(897,252,126,84).exists(Pattern("SKIP.png").similar(0.50))):
+                        Region(897,252,126,84).click(Pattern("SKIP.png").similar(0.50))
+            wait(2)
         Region(310,142,107,98).click(Pattern("MyPage.png").similar(0.90))
         wait(2)
     #Go like a gacha and come back
