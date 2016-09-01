da240d6bbf734e6a952c3c0b242d8b3880e4de90
Joey Chung K <joeychung960@gmail.com>
2016-08-31 23:33:34 -0700

    Added some extra images in. Modified timing on like system. Current crashes include no safe checking of the 'Get Help' button as well as when servers crash. I need to add a refresh and restart button in the event that happens.

--- IW.sikuli/IW.py
+++ IW.sikuli/IW.py
@@ -7,12 +7,19 @@ while True:
         if(Region(595,325,246,362).exists(Pattern("New.png").similar(0.95))):
             newText = find(Pattern("New.png").similar(0.95))
             newRegion = Region(newText.getX(),newText.getY(),220,155)
+            #Need to make an escape case here
             newRegion.click("GoHelp.png")
             if(Region(597,561,237,98).exists("Persuade.png")):
                 Region(597,561,237,98).click("Persuade.png")
                 wait(3)
                 if(Region(897,252,126,84).exists(Pattern("SKIP.png").similar(0.50))):
                     Region(897,252,126,84).click(Pattern("SKIP.png").similar(0.50))
+                else:
+                    continue
+            else:
+                continue
+        else:
+            continue
         wait(2)
         Region(310,142,107,98).click(Pattern("MyPage.png").similar(0.90))
         wait(2)
@@ -27,7 +34,7 @@ while True:
         wait(5)
         Region(547,310,228,220).onVanish(Pattern("Connecting.png").similar(0.85),wait(.1))
         dragDrop(Location(995,340),Location(995,515))
-        wait(.5)
+        wait(2)
         if(Region(850,334,156,126).exists("Like.png")):
             click("Like.png")
         wait(2)
