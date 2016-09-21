5a3880801908a0ed68a1813ed93f16712a4a3e58
Joey Chung K <joeychung960@gmail.com>
2016-09-21 16:24:49 -0700

    Modified a LOT of stuff in the script, cleaned it up slightly. Text capture/recognition does not work atm with names. Region capturing to text() yields 'no text' so that will have to be worked around.

--- IW.sikuli/IW.py
+++ IW.sikuli/IW.py
@@ -5,40 +5,40 @@ while True:
         wait(10)
         if(Region(513,595,328,120).exists("StartName.png")):
             click("StartName.png")
+            click("StartName.png")
             wait(5)            
     #Look for new co-ops
-    if(Region(441,400,81,76).exists(Pattern("NewComboCoOp.png").similar(0.80))):
-        wait(.1)
+    if(Region(441,400,81,76).exists(Pattern("NewComboCoOp.png").similar(0.80),2)):
         Region(441,400,81,76).click("NewComboCoOp.png")
         if(Region(595,325,246,362).exists(Pattern("New.png").similar(0.95))):
             newText = find(Pattern("New.png").similar(0.95))
             newRegion = Region(newText.getX(),newText.getY(),220,155)
             #Need to make an escape case here
-            if(newRegion.exists("GoHelp.png")):
+            if(newRegion.exists("GoHelp.png"),2):
                 newRegion.click("GoHelp.png")
-                if(Region(597,561,237,98).exists("Persuade.png")):
-                    Region(597,561,237,98).click("Persuade.png")
-                    wait(3)
-                    if(Region(897,252,126,84).exists(Pattern("SKIP.png").similar(0.50))):
-                        Region(897,252,126,84).click(Pattern("SKIP.png").similar(0.50))
+                if(Region(597,561,237,98).exists("Persuade.png"),2):
+                    click("Persuade.png")
+                    if(Region(897,252,126,84).exists(Pattern("SKIP.png").similar(0.50)),3):
+                        click(Pattern("SKIP.png").similar(0.50))
             wait(2)
-        Region(310,142,107,98).click(Pattern("MyPage.png").similar(0.90))
-        wait(2)
+        if(Region(310,142,107,98).exists(Pattern("MyPage.png").similar(0.90),2)):
+                click(Pattern("MyPage.png").similar(0.90))
+                wait(2)
     #Go like a gacha and come back
     else:
         wait(.1)
         hover(Location(570,185))
-        if(Region(510,226,111,90).exists("MainGacha.png")):
+        if(Region(510,226,111,90).exists("MainGacha.png",2)):
             click("MainGacha.png")
-        else:
-            Region(325,234,62,61).click("SideGacha.png")
-        wait(5)
-        Region(547,310,228,220).onVanish(Pattern("Connecting.png").similar(0.85),wait(.1))
-        dragDrop(Location(995,340),Location(995,515))
+        elif(Region(325,234,62,61).exists("SideGacha.png",2)):
+            click("SideGacha.png")
         wait(2)
-        if(Region(850,334,156,126).exists("Like.png")):
-            click("Like.png")
-        wait(2)
-        Region(547,310,228,220).onVanish(Pattern("Connecting.png").similar(0.85))
-        Region(310,142,107,98).click(Pattern("MyPage.png").similar(0.90))
+        if(Region(416,230,92,36).exists("GachaText.png",20)):
+            wait(2)
+            dragDrop(Location(995,340),Location(995,515))
+            if(Region(850,334,156,126).exists(Pattern("Like.png").similar(0.50),2)):
+                click(Pattern("Like.png").similar(0.50))
+                wait(2)
+        if(Region(310,142,107,98).exists(Pattern("MyPage.png").similar(0.98),5)):
+            click(Pattern("MyPage.png").similar(0.90))
         wait(1)
