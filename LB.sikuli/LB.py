69d668c21df6c8d5043216568c8f9194b1266fbb
Joey Chung K <joeychung960@gmail.com>
2016-10-08 15:32:01 -0700

    Accidentally deleted, but re-added Nanami's image. Added a method for looping through a Line as well as the method to delete an entire row of units.

--- LB.sikuli/LB.py
+++ LB.sikuli/LB.py
@@ -1,23 +1,53 @@
-def replaceRinka():
-    #Replace Rinka with NSeira
-    return -1
+def changeLine(arr):
+    changeX = 607
+    changeY = 643
+    for char in arr:
+        unitFormReg.wait("UnitFormation.png",20)
+        click(Location(changeX,changeY))
+        changeX += 109
+        textReg.wait("ChangeIdols.png",20)
+        if selectReg.exists(char):
+            selectChar(char)
 
 def selectChar(img):
-    match = selectRegion.find(img)
-    miniRegion = Region(match.x,match.y,match.w,match.h+40)
-    wait(1)
-    miniRegion.click("SmallChangeBtn.png")
-
-    
-#"LargeChangeBtn.png""NSeira.png""Rinka.png"Pattern("DeleteBtn.png").similar(0.88)"FrontTab.png""RearTab.png""SmallChangeBtn.png"
-front = ["DivaMomona.png","SukuMomona.png","1475924352569.png","Fuka.png"]
-rear = ["Urara.png","Chiyo.png","Ryoko.png","Masumi.png"]
-selectRegion = Region(426,283,576,264)
-for char in front:
     #Move the mouse away from the tooltips
+    textReg.wait("ChangeIdols.png",20)
     hover(Location(570,185))
-    wait(3)
-#    hover(char)
-    if selectRegion.exists(char):
-        selectChar(char)
-    wait(3)
+    match = selectReg.find(img)
+    miniReg = Region(match.x,match.y,match.w,match.h+40)
+    wait(1)
+    miniReg.click("SmallChangeBtn.png")
+
+def selectSlot():
+    #Deprecated to reduce computation power
+    #changeLine() sequentially changes all units in a Line
+    return -1
+
+def deleteRow():
+    deleteX = 626
+    deleteY = 643
+    for i in range(4):
+        unitFormReg.wait("UnitFormation.png",20)
+        click(Location(deleteX,deleteY))
+        deleteX += 109
+        wait(2)
+
+def replaceRinka():
+    #Replace Rinka with NSeira
+    
+    #Go to Last section
+    click(769,672)
+    selectChar("NSeira.png")
+
+def replaceNSeira():
+    #Replace NSeira with Rinka
+    return -1
+#"LargeChangeBtn.png""Rinka.png"Pattern("DeleteBtn.png").similar(0.88)"FrontTab.png""RearTab.png""SmallChangeBtn.png""EmptySlot.png"
+front = ["DivaMomona.png","SukuMomona.png","Nanami.png","Fuka.png"]
+rear = ["Urara.png","Chiyo.png","Ryoko.png","Masumi.png"]
+selectReg = Region(426,283,576,264)
+unitFormReg = Region(415,434,136,38)
+textReg = Region(532,236,126,26)
+
+deleteRow()
+changeLine(front)
