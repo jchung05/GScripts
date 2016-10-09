954ddba0ec6f84ad9341a27fc2f55381906c2a08
Joey <joeychung960@gmail.com>
2016-10-09 04:48:10 -0700

    Added a bunch of stuff I don't remember from a train ride earlier. This should be done, but needs testing.

--- LB.sikuli/LB.py
+++ LB.sikuli/LB.py
@@ -1,4 +1,4 @@
-def changeLine(arr):
+def changeRow(arr):
     changeX = 607
     changeY = 643
     for char in arr:
@@ -23,6 +23,9 @@ def selectSlot():
     #changeLine() sequentially changes all units in a Line
     return -1
 
+def changeTab(img):
+    lineReg.click(img)
+
 def deleteRow():
     deleteX = 626
     deleteY = 643
@@ -34,20 +37,37 @@ def deleteRow():
 
 def replaceRinka():
     #Replace Rinka with NSeira
-    
+    click(Location(1,1)) #Click Change
+    textReg.wait("ChangeIdols.png",20)
     #Go to Last section
     click(769,672)
-    selectChar("NSeira.png")
+    textReg.wait("ChangeIdols.png",20)
+    if selectReg.exists("NSeira.png"):
+        selectChar("NSeira.png")
 
 def replaceNSeira():
     #Replace NSeira with Rinka
-    return -1
+    click(Location(1,1))
+    textReg.wait("ChangeIdols.png",20)
+    if selectReg.exists("Rinka.png"):
+        selectChar("Rinka.png")
 #"LargeChangeBtn.png""Rinka.png"Pattern("DeleteBtn.png").similar(0.88)"FrontTab.png""RearTab.png""SmallChangeBtn.png""EmptySlot.png"
 front = ["DivaMomona.png","SukuMomona.png","Nanami.png","Fuka.png"]
 rear = ["Urara.png","Chiyo.png","Ryoko.png","Masumi.png"]
 selectReg = Region(426,283,576,264)
 unitFormReg = Region(415,434,136,38)
 textReg = Region(532,236,126,26)
+lineReg = Region(1,1,1,1)
 
-deleteRow()
-changeLine(front)
+#Default start at front line
+deleteRow() #delete Front Line
+changeTab("RearTab.png") #switch tabs
+deleteRow() #delete Rear Line
+replaceRinka() #replace Rear Leader
+sleep(20) #Wait time to select opponents
+#I may replace the preset wait times with an eternal wait on a key press
+replaceNSeira() #put Rear Leader back in
+changeRow(rear) #place all Rear Line back in
+changeTab("FrontTab.png") #switch tabs
+changeRow(front) #place all Front Line back in
+sleep(30) #Go fight!
