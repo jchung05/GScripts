693e6c20e4b42ddc5a66169deaf6cf90f9e3031f
Joey Chung K <joeychung960@gmail.com>
2016-10-12 16:14:10 -0700

    Very basic script that starts from start menu and loops Gacha x 10 until it can't anymore then exits.

---
+++ Gacha.sikuli/Gacha.py
@@ -0,0 +1,34 @@
+#The first draw 10 will need to be manually done
+#Then this script will loop until it cannot draw anymore
+#"DrawAgain.png"
+def skipAndClick():
+    Region(864,239,190,114).wait("Skip.png",20)
+    Region(864,239,190,114).click("Skip.png")
+    wait(3)
+    click(Location(715,470))
+    Region(813,620,184,77).wait("DrawAgain.png",20)
+
+def drawAgain():
+    click("DrawAgain.png")
+    wait(1)
+    if Region(614,434,207,78).exists("Draw10.png"):
+        Region(614,434,207,78).click("Draw10.png")
+    else:
+        exit
+
+def firstDraw():
+    Region(527,232,79,83).click("MainGacha.png")
+    Region(414,264,605,48).wait("Normal.png",20)
+    Region(414,264,605,48).click("Normal.png")
+    Region(414,264,605,48).wait("NormalSelected.png",20)
+    if Region(717,456,262,69).exists("Draw10.png"):
+        Region(717,456,262,69).click("Draw10.png")
+    else:
+        exit
+
+firstDraw()
+skipAndClick()
+wait(5)
+while(Region(813,620,184,77).exists("DrawAgain.png")):
+    drawAgain()
+    skipAndClick()
